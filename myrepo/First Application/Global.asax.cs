558327b3be0d58e66f18f36697d20b07c62b5ed3
augsongg1234 <augsongg@gmail.com>
2016-11-22 21:22:01 +1100

    myfirst

--- myrepo/First Application/Global.asax.cs
+++ myrepo/First Application/Global.asax.cs
@@ -12,7 +12,7 @@ namespace First_Application
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes); 
         }
     }
 }
