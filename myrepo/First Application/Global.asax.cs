f4aa5297b68c1f94859473412842426d56fadfd4
augsongg1234 <augsongg@gmail.com>
2016-11-22 21:39:03 +1100

    mybrnach commit

--- myrepo/First Application/Global.asax.cs
+++ myrepo/First Application/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +7,7 @@ using System.Web.Routing;
 
 namespace First_Application
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplications : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
